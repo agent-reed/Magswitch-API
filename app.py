1a5c5d3ff885bef16c57e747daaa9b63ba982382
Andrew Gentry <agentdevr@gmail.com>
2016-05-04 14:45:05 -0600

    just when

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def checkLogin():
 		cur = con.cursor()
 		print("created cursor")
 
-		cur.execute("SELECT psswrd FROM users WHERE \"email\" = '%s'", email)
+		cur.execute("SELECT psswrd, firstName FROM users WHERE \"email\" = %s", email)
 		print("executed selection")
 		results = cur.fetchone()
 		print("Stored results")
