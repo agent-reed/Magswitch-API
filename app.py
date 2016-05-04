c1d8ecb75580982ed43925ad8fd0cdea476f1fcc
Andrew Gentry <agentdevr@gmail.com>
2016-05-04 14:46:57 -0600

    you think

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def checkLogin():
 		con = createDBConnection()
 		cur = con.cursor()
 		print("created cursor")
-
+		print(email)
 		cur.execute("SELECT psswrd, firstName FROM users WHERE \"email\" = %s", email)
 		print("executed selection")
 		results = cur.fetchone()
