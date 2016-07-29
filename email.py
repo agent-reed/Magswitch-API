a0d956e2cb9644a7ffe6decff122f00b4f6f04e4
Andrew Gentry <agentdevr@gmail.com>
2016-07-29 15:45:53 -0600

    Removed LastEntry

--- email.py
+++ email.py
@@ -36,9 +36,9 @@ for i in logins:
 	someList.append(i[0])
 newCount = sum(someList)
 
-cur.execute("SELECT logins, entry FROM stats ORDER BY entry DESC LIMIT 1")  #while we're in the stats table might as well grab the last entry too
+cur.execute("SELECT logins, entry FROM stats")  #while we're in the stats table might as well grab the last entry too
 lastCountList = cur.fetchall()
-lastEntry = lastCount[1]
+lastEntry = 5
 
 countList = []
 for i in lastCountList:
