f677b66ce4998b743b56d0f1696b1a76208b0839
Andrew <agentdevr@gmail.com>
2016-07-29 12:28:36 -0600

    Interpolate the product into email for testing & POC

--- email.py
+++ email.py
@@ -5,9 +5,10 @@ con = db.createDBConnection()
 cur = con.cursor()
 #reset the weekly views of the products after grabbing the highest. This script runs once a week.
 cur.execute("SELECT name FROM products ORDER BY weeklyviews DESC LIMIT 1")
-weeklyproduct = cur.fethone()
+weeklyproduct = cur.fetchone()
 
 
-os.system("echo \"This is a test email from the app server\" | mail -s \"%s\" agentry@magswitch.com.au", (weeklyproduct,))
+os.system("echo \"This is a test email from the app server\" | mail -s \"Weekly product:  %s \" agentry@magswitch.com.au"%(weeklyproduct))
+
 
 
