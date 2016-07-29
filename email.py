e749e154f7dbccbdae8f96cc2d199fc6142b3bff
Andrew Gentry <agentdevr@gmail.com>
2016-07-29 12:44:59 -0600

    Added new users stat for testing.

--- email.py
+++ email.py
@@ -3,12 +3,26 @@ import db
 
 con = db.createDBConnection()
 cur = con.cursor()
-#reset the weekly views of the products after grabbing the highest. This script runs once a week.
+
+#grab the product name with the highest weekly views
 cur.execute("SELECT name FROM products ORDER BY weeklyviews DESC LIMIT 1")
 weeklyproduct = cur.fetchone()
 
+#reset the weekly views of the products after grabbing the highest. This script runs once a week.
+cur.execute("UPDATE products SET weeklyviews = 0")
+con.commit()
 
-os.system("echo \"This is a test email from the app server\" | mail -s \"Weekly product:  %s \" agentry@magswitch.com.au"%(weeklyproduct))
+
+cur.execute("SELECT userid FROM users ORDER BY userid DESC LIMIT 1")
+thisHigh = cur.fetchone()
+
+cur.execute("SELECT newusers FROM stats ORDER BY newusers DESC LIMIT 1")
+oldHigh = cur.fetchone()
+
+newusers = thisHigh - oldHigh
+
+
+os.system("echo \"This week, the most popular product was: %s \n Number of new users : %s \" | mail -s \"Weekly Stat Update\" agentry@magswitch.com.au"%(weeklyproduct, newusers))
 
 
 
