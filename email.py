a1a37b84f7080344120e92367a58461fead3e924
Andrew Gentry <agentdevr@gmail.com>
2016-07-29 14:57:57 -0600

    reverted back

--- email.py
+++ email.py
@@ -19,8 +19,8 @@ thisHigh = cur.fetchone()
 
 #grab the highest userid that has been stored in stats.  we know the difference between the two is the number of new users. 
 cur.execute("SELECT newusers FROM stats")
-oldHigh = cur.fetchall()
-newusers = thisHigh[0] - sum(oldHigh[0])
+oldHigh = cur.fetchone()
+newusers = thisHigh[0] - oldHigh[0]
 print(newusers)
 
 cur.execute("SELECT logincount FROM users")
@@ -33,14 +33,14 @@ for i in logins:
 newCount = sum(someList)
 
 cur.execute("SELECT logins, entry FROM stats")  #while we're in the stats table might as well grab the last entry too
-lastCount = cur.fetchall()
+lastCount = cur.fetchone()
 lastEntry = lastCount[1]
-weeklyLogins = newCount - sum(lastCount[0])
+weeklyLogins = newCount - lastCount[0]
 print(weeklyLogins)
 
 #each time we run this script we want to post it's results in the stats table and stamp the date on it
 date = time.strftime("%c")
-cur.execute("INSERT INTO stats VALUES(%s, %s, %s, %s, %s)" ,(lastEntry+1, date, newusers, logins, weeklyproduct))
+cur.execute("INSERT INTO stats VALUES(%s, %s, %s, %s, %s)" ,(lastEntry+1, date, newusers, weeklyLogins, weeklyproduct))
 con.commit()
 
 os.system("echo \"Hey everybody! Here are the statistics for the Mobile App over the past week.\n\nMost Viewed Product: %s \n\nNumber of new users: %s \n\nNumber of logins: %s\" | mail -s \"Weekly Stat Update\" agentry@magswitch.com.au"%(weeklyproduct[0], newusers, weeklyLogins))
