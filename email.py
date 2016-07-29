de2678eb349baa51818f1d738ef2ac07daaf758c
Andrew Gentry <agentdevr@gmail.com>
2016-07-29 12:13:57 -0600

    Insert Weekly product into email

--- email.py
+++ email.py
@@ -1,5 +1,13 @@
 import os
+import db
 
-os.system("echo \"This is a test email from the app server\" | mail -s \"Another Test Email\" agentry@magswitch.com.au")
+con = db.createDBConnection()
+cur = con.cursor()
+#reset the weekly views of the products after grabbing the highest. This script runs once a week.
+cur.execute("SELECT name FROM products ORDER BY weeklyviews DESC LIMIT 1")
+weeklyproduct = cur.fethone()
+
+
+os.system("echo \"This is a test email from the app server\" | mail -s \"%s\" agentry@magswitch.com.au", (weeklyproduct,))
 
 
